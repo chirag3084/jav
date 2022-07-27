bda4785e2dc67dfcedb051547842463ff0a00beb
chirag3084 <110027137+chirag3084@users.noreply.github.com>
2022-07-27 21:25:18 +0530

    Create Random number.java

---
+++ Random number.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int mynumber = (int)(Math.random*100);
+	
+		usernumber = 0;
+		do {
+			System.out.println("guess my number : ");
+			
+			usernumber = sc.nextInt();
+			
+			if(usernumber == mynumber) {
+				System.out.println("your answer is correct  ! ");
+			}
+			
+			else if(usernumber > mynumber) {
+				System.out.println("your number is big");
+			}
+			
+			else {
+				System.out.println("your numbe r is")
+				
+			
+	
+	
+	
+	}
+}
