6412889c5d6c7b546afe4a823b4bc33c23a25092
chirag3084 <110027137+chirag3084@users.noreply.github.com>
2022-07-31 23:14:51 +0530

    Create Taking input from user.java

---
+++ Taking input from user.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+ public static void main(String[] args) {
+
+  System.out.println("input from user");
+
+  Scanner sc =   new Scanner(System.in);
+
+  System.out.println("enter your number : ");
+
+  int a = sc.nextInt();
+
+  System.out.println("enter your second number : ");
+
+  int b = sc.nextInt();
+
+  int sum = a+b;
+
+  System.out.println("the sum of those numbers is : ");
+
+  System.out.println(sum);
+
+  
+
+  
+
+ }
+
+}
