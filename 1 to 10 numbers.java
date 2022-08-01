07b499e7bff0462a24ed116aa9dd309bc6cd507d
chirag3084 <110027137+chirag3084@users.noreply.github.com>
2022-08-01 16:35:54 +0530

    Create 1 to 10 numbers.java

---
+++ 1 to 10 numbers.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args ) {
+        int val = 1;
+        int val1 = 11;
+        for(val = 1;val < val1;val++)
+        System.out.println(val );
+    }
+}
