0dfd74e709408012bb64736a4bba90c964833432
chirag3084 <110027137+chirag3084@users.noreply.github.com>
2022-08-02 23:32:24 +0530

    Create Pass or fail in exam.java

---
+++ Pass or fail in exam.java
@@ -0,0 +1,9 @@
+public class Main {
+public static void main(String[] args) {
+ int marks = 23;
+if(marks >= 33)
+System.out.println("you are pass");
+else
+System.out.println("you are fail");
+}
+}
